a3ed4fb4306aa471b646731c08fb3cd1148f0e19
Sb1947 <82405830+Sb1947@users.noreply.github.com>
2021-08-13 17:32:37 -0400

    Add files via upload

---
+++ Boggie.js
@@ -0,0 +1,25 @@
+class Boggie 
+{
+    constructor(x,y,width,height)
+    {
+        var options=
+        {
+            isStatic:false
+        }
+        this.width = width;
+        this.height = height
+        this.body = Bodies.rectangle(x,y,width,height,options)
+        this.image = loadImage("images/coach.png")
+        World.add(myWorld,this.body)
+        Matter.Body.setMass(this.body,this.body.mass*5)
+    }
+    display()
+    {
+        imageMode(CENTER)
+        image(this.image,this.body.position.x,this.body.position.y,this.width,this.height) 
+
+    }
+
+
+
+}
---
+++ Rock.js
@@ -0,0 +1,28 @@
+class Rock 
+{
+    constructor(x,y,width,height)
+    {
+        var options=
+        {
+            isStatic:false
+        }
+        this.width = width;
+        this.height = height
+        this.body = Bodies.rectangle(x,y,width,height,options)
+        this.image = loadImage("images/rock1.png")
+        World.add(myWorld,this.body)
+        Matter.Body.setMass(this.body,this.body.mass*5)
+    }
+    display()
+    {
+
+        imageMode(CENTER)
+        fill("black")
+        stroke("grey")
+        image(this.image,this.body.position.x,this.body.position.y,this.width,this.height) 
+
+    }
+
+
+
+}
